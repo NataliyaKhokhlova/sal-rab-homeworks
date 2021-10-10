2f213d88b2a5639ee264a0506792d22c0dfef773
Nataliya Khokhlova <9162603119x@gmail.com>
2021-10-10 10:51:57 +0300

    task 1 is amended

--- task_1/solution.js
+++ task_1/solution.js
@@ -50,8 +50,8 @@ function addInCartSumChange(sum, delta) {
     // Задание №1.3. Увеличение суммы в корзине
 let newSum;
 newSum=oldSum+difference;
-newSumText=newSum + '₽';
-newSumText='${newSum}₽';
+newSumText=newSum + ' ₽';
+newSumText=`${newSum} ₽`;
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
