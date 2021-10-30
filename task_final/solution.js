2fcfe63c795f51cb61d3ce46064ed4273756b62b
Nataliya Khokhlova <9162603119x@gmail.com>
2021-10-30 19:48:50 +0300

    Final task is ready

--- task_final/solution.js
+++ task_final/solution.js
@@ -35,14 +35,14 @@
 
 function sendRequest(name, phone, address, goods, sum) {
 
-    let data = {client: '', order: {address: '', sum: ''}, goods: [{title: '', count: ''}]};
+    let data = {client: '', order: {address: '', sum: ''}, goods: []};
     let countOfGoods = goods.length; 
-    for (let i = 0; i = countOfGoods; i++) {
+    for (let i = 0; i < countOfGoods; i++) {
     data.goods.push({title: goods[i].title, count: goods[i].count});
-    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
+    }
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
     data.order.sum = sum;
-    data.client = `${name}, ${phone}`;
+    data.client = `${name} ${phone}`;
     let jsonData = JSON.stringify({data: data});
     return jsonData;
-    }
 }
